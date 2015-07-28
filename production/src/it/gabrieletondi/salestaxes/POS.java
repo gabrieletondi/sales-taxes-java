9b4691ac3205ffb6f56159901ffc44e3eb6e42a7
Gabriele Tondi <gabriele.tondi@xpeppers.com>
2015-07-28 21:16:30 +0200

    can sell multiple not imported items

--- production/src/it/gabrieletondi/salestaxes/POS.java
+++ production/src/it/gabrieletondi/salestaxes/POS.java
@@ -2,35 +2,53 @@ package it.gabrieletondi.salestaxes;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class POS {
 
     private final InMemoryWithDefaultTaxPolicy taxPolicy;
-    private SaleItem saleItem;
+    private List<SaleItem> saleItems;
 
     public POS(InMemoryWithDefaultTaxPolicy taxPolicy) {
         this.taxPolicy = taxPolicy;
+        this.saleItems = new ArrayList<SaleItem>();
     }
 
     public void sell(String sellCommand) {
-        saleItem = SaleItem.fromSellCommand(sellCommand);
+        saleItems.add(SaleItem.fromSellCommand(sellCommand));
     }
 
     public String receipt() {
-        BigDecimal netPrice = saleItem.getNetPrice();
-        Tax tax = taxPolicy.forItemName(saleItem.getProductName());
-        BigDecimal taxAmount = tax.dutyAmount(netPrice);
+        String receipt = "";
+        BigDecimal total = BigDecimal.ZERO;
+        BigDecimal salesTaxes = BigDecimal.ZERO;
 
-        BigDecimal taxedPrice = netPrice.add(taxAmount);
-        BigDecimal salesTaxes = taxedPrice.subtract(saleItem.getNetPrice());
+        for (SaleItem saleItem : saleItems) {
+            BigDecimal netPrice = saleItem.getNetPrice();
+            Tax tax = taxPolicy.forItemName(saleItem.getProductName());
+            BigDecimal taxAmount = tax.dutyAmount(netPrice);
+
+            BigDecimal taxedPrice = netPrice.add(taxAmount);
+            total = total.add(taxedPrice);
+            salesTaxes = salesTaxes.add(taxAmount);
+
+            DecimalFormat decimalFormat = new DecimalFormat();
+            decimalFormat.setMaximumFractionDigits(2);
+            decimalFormat.setMinimumFractionDigits(2);
+
+            receipt += saleItem.getQuantity() + " " + saleItem.getProductName() + ": " + decimalFormat.format(taxedPrice) + "\n";
+
+        }
 
         DecimalFormat decimalFormat = new DecimalFormat();
         decimalFormat.setMaximumFractionDigits(2);
         decimalFormat.setMinimumFractionDigits(2);
 
-        return saleItem.getQuantity() + " " + saleItem.getProductName() + " : " + decimalFormat.format(taxedPrice) + "\n" +
-                "Sales Taxes: " + decimalFormat.format(salesTaxes) + "\n" +
-                "Total: " + decimalFormat.format(taxedPrice);
+        receipt += "Sales Taxes: " + decimalFormat.format(salesTaxes) + "\n" +
+                "Total: " + decimalFormat.format(total);
+
+        return receipt;
     }
 
 }
--- tests/test/it/gabrieletondi/salestaxes/POSTest.java
+++ tests/test/it/gabrieletondi/salestaxes/POSTest.java
@@ -22,7 +22,7 @@ public class POSTest {
 
         String receipt = pos.receipt();
 
-        String expected = "1 book : 12.49\n" +
+        String expected = "1 book: 12.49\n" +
                 "Sales Taxes: 0.00\n" +
                 "Total: 12.49";
         assertEquals(expected, receipt);
@@ -34,9 +34,25 @@ public class POSTest {
 
         String receipt = pos.receipt();
 
-        String expected = "1 musical CD : 16.49\n" +
+        String expected = "1 musical CD: 16.49\n" +
                 "Sales Taxes: 1.50\n" +
                 "Total: 16.49";
+        assertEquals(expected, receipt);
+    }
+
+    @Test
+    public void sellMultipleNotImportedItems() throws Exception {
+        pos.sell("1 book at 12.49");
+        pos.sell("1 music CD at 14.99");
+        pos.sell("1 chocolate bar at 0.85");
+
+        String receipt = pos.receipt();
+
+        String expected = "1 book: 12.49\n" +
+                "1 music CD: 16.49\n" +
+                "1 chocolate bar: 0.85\n" +
+                "Sales Taxes: 1.50\n" +
+                "Total: 29.83";
         assertEquals(expected, receipt);
     }
 
